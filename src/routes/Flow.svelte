74241a434b09052f3205eeaf84a686ec2ce6c6cc
Bray Polkinghorne <braypolk@gmail.com>
2023-11-27 12:01:59 -0800

    save and restore from file

--- src/routes/Flow.svelte
+++ src/routes/Flow.svelte
@@ -35,23 +35,57 @@
     }
   }
 
-  function onSave() {
-    console.log(toObject());
-    localStorage.setItem("rake-test-flow", JSON.stringify(toObject()));
-  }
+  async function onSave() {
+  const flow = toObject();
+  console.log(flow);
+  
+  try {
+    const response = await fetch('/api/flow', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({flow: flow, location: '/Users/braypolkinghorne/Documents/code/Rake/rake-app/src/lib/test.json'})  //todo: will have to be changed when not using local storage
+    });
 
-  async function onRestore() {
-    console.log("in restore");
-    const flow = JSON.parse(localStorage.getItem("rake-test-flow"));
-    console.log(flow);
-
-    if (flow) {
-      const { x = 0, y = 0, zoom = 1 } = flow.viewport;
-      $nodes = flow.nodes;
-      $edges = flow.edges;
-      setViewport({ x, y, zoom });
+    if (response.ok) {
+      console.log('Flow saved successfully');
+    } else {
+      console.error('Failed to save flow');
     }
+  } catch (error) {
+    console.error('Error saving flow:', error);
   }
+}
+
+async function onRestore() {
+  try {
+    //todo: will have to be changed when not using local storage
+    const response = await fetch('/api/flow?location=/Users/braypolkinghorne/Documents/code/Rake/rake-app/src/lib/test.json');
+    
+    if (response.ok) {
+      console.log('Flow restored successfully');
+      const res = await response.json();
+      const { flow, message } = res.body;
+      console.log(flow, message);
+      
+
+      if (flow) {
+        const { x = 0, y = 0, zoom = 1, nodes, edges } = flow;
+        $nodes = nodes;
+        $edges = edges;
+        setViewport({ x, y, zoom });
+        console.log(message);
+      } else {
+        console.log(message);
+      }
+    } else {
+      console.error('Failed to restore flow');
+    }
+  } catch (error) {
+    console.error('Error restoring flow:', error);
+  }
+}
 </script>
 
 <main>
---
+++ src/routes/api/flow/+server.js
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import { resolve } from 'path';
+
+export async function POST({ request }) {
+    try {
+        let {flow, location} = await request.json();
+
+        // Perform the file save operation here
+        fs.writeFileSync(location, JSON.stringify(flow));
+
+        return new Response(JSON.stringify({
+            status: 200,
+            body: { message: 'Flow saved successfully' }
+        }));
+    } catch (error) {
+        console.error('Error saving flow:', error);
+
+        return new Response(JSON.stringify({
+            status: 500,
+            body: { error: 'Failed to save flow' }
+        }));
+    }
+}
+
+// src/routes/api/restoreFlow.json.js
+
+export async function GET({url}) {
+    try {
+        const filePath = resolve(url.searchParams.get('location'));
+
+        if (!fs.existsSync(filePath)) {
+            return new Response(JSON.stringify({
+                status: 200,
+            body: { message: 'No flow data found' }
+          }));
+        }
+    
+        const fileData = fs.readFileSync(filePath, 'utf8');
+        const flow = JSON.parse(fileData);
+
+        if (flow) {
+            const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+            const nodes = flow.nodes;
+            const edges = flow.edges;
+
+            return new Response(JSON.stringify({
+                status: 200,
+                body: {
+                    message: 'Flow restored successfully',
+                    flow: { x, y, zoom, nodes, edges }
+                }
+            }));
+        } else {
+            return new Response(JSON.stringify({
+                status: 200,
+                body: { message: 'No flow data found' }
+            }));
+        }
+    } catch (error) {
+        console.error('Error restoring flow:', error);
+
+        return new Response(JSON.stringify({
+            status: 500,
+            body: { error: 'Failed to restore flow' }
+        }));
+    }
+}
